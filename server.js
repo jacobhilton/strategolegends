8b6bdc6d739fd36143e9a8e7b8a0794fe67bd9e5
Jacob Hilton <jacob.hilton@gmail.com>
2015-01-03 08:52:57 +0000

    initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express=require("express");
 var app=express();
 app.set("port",(process.env.PORT||5000));
 app.use(express.static(__dirname+"/public"));
-var io=require("socket.io").listen(3000);
+var io=require("socket.io").listen(process.env.PORT||5000);
 var legendsconstructor=require("./public/legends.js");
 var games=[];
 io.sockets.on("connection",function(socket){
