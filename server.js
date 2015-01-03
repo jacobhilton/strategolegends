cbc1bfc83e7424a0a5083543e2af130a7a28574b
Jacob Hilton <jacob.hilton@gmail.com>
2015-01-03 09:05:14 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express=require("express");
 var app=express();
 var http=require("http").Server(app);
-var io=require('socket.io')(http);
+var io=require("socket.io")(http);
 app.use(express.static(__dirname+"/public"));
-http.listen(process.env.PORT||5000);
 var legendsconstructor=require("./public/legends.js");
 var games=[];
-io.sockets.on("connection",function(socket){
+io.on("connection",function(socket){
   socket.emit("gameslist",(function(){
     var returnvalue=[];
     for(var gameid=0;gameid<games.length;gameid++){
@@ -48,3 +47,4 @@ io.sockets.on("connection",function(socket){
   });
   socket.on("disconnect",function(){});
 });
+http.listen(process.env.PORT||5000);
