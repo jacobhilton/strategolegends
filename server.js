9c8d7ad34b20dfb63b88df9716a77117f734f568
Jacob Hilton <jacob.hilton@gmail.com>
2015-01-08 17:22:16 +0000

    initial commit

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ var pg = require('pg');
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
+    client.query('SELECT * FROM data', function(err, result) {
+      console.log('done');
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
