15da56bf6866ff1b7ccbaa8966943db8676174b8
Jacob Hilton <jacob.hilton@gmail.com>
2015-01-12 13:37:04 +0000

    initial commit

--- public/legends.js
+++ public/legends.js
@@ -10,7 +10,7 @@ var legendsconstructor=function(){
       "waitforreply":true,
       "ping":function(){
         var image=new Image();
-        img.src=imagedirectory+"empty.gif?timestamp="+(new Date()).getTime();
+        image.src=legends.server.imagedirectory+"empty.gif?timestamp="+(new Date()).getTime();
       }
     },
 
